46a97d0eccd39bb14b6572bdb805c7c9361286b5
ynoynaert <youna.noynaert@telenet.be>
2024-09-25 17:55:04 +0200

    Homepage update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,109 +1,114 @@
-import CardRow from "../components/CardRow";
-import { Flex, Box } from "@chakra-ui/react";
+import {
+  Flex,
+  Card,
+  CardBody,
+  Image,
+  Stack,
+  Heading,
+  Text,
+  Button,
+} from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <Flex direction="column" align="center" justify="center" w="100%" p={4}>
-      {/* Levensloop Card */}
-      <Box maxW="60%" width="100%" mb={4}>
-        <CardRow
-          img={"/images/home/levensloop2024.jpg"}
-          imgalt={"Levensloop"}
-          imgsize={{ base: "100%", md: "350px" }} // Responsive image size
-          title={"Levensloop 2024"}
-          desc={
-            "Op zaterdag 28 en zondag 29 gaat Levensloop door in het stadspark van Aalst. Ook met de (fan)club nemen we hieraan deel."
-          }
-          but={"Meer info"}
-          butLink={"/levensloop"}
-        />
-      </Box>
-
-      {/* Gouden Kriekskes Card */}
-      <Box maxW="60%" width="100%" mb={4}>
-        <CardRow
-          img={"/images/home/gouden-kriekskes-afbeelding.jpg"}
-          imgalt={"Gouden Kriekskes"}
-          imgsize={{ base: "100%", md: "450px" }} // Responsive image size
-          title={"Gouden Kriekskes 2024"}
-          desc={
-            "Jaarlijks sluiten we het seizoen af met de uitreiking van prijzen beste speler, beste jongere, meest enthousiaste speler en Lifetime Achievement Award."
-          }
-          but={"Ontdek de winnaars"}
-          butLink={"/goudenkriekskes"}
-        />
-      </Box>
-
-      {/* Blocknotjen Card */}
-      <Box maxW="60%" width="100%" mb={4}>
-        <CardRow
-          img={"/images/home/021-2024-mei.jpg"}
-          imgalt={"Blocknotjen"}
-          imgsize={{ base: "100%", md: "200px" }} // Responsive image size
-          title={"'t Blocknotjen"}
-          desc={
-            "We hebben ook een eigen ledenblad: 't Blocknotjen. Tijdens het seizoen verschijnt dit 1 keer per maand."
-          }
-          but={"Lees online"}
-          butLink={"/blocknotjen"}
-        />
-      </Box>
-
-      {/* Lindemans Aalst Website - External Link */}
-      <Box maxW="60%" width="100%" mb={4}>
-        <CardRow
-          img={"/images/home/website-lindemans-aalst.png"}
-          imgalt={"Website Lindemans Aalst"}
-          imgsize={{ base: "100%", md: "200px" }} // Responsive image size
-          desc={"Meer informatie over onze club vind je op deze website."}
-          but={"Meer info"}
-          butLink={"https://www.lindemansaalst.be"}
-          isExternal={true} // This is an external link
-        />
-      </Box>
-
-      {/* Lotto Volley League - External Link */}
-      <Box maxW="60%" width="100%" mb={4}>
-        <CardRow
-          img={"/images/home/website-volley-league.png"}
-          imgalt={"Website Lotto Volley League"}
-          imgsize={{ base: "100%", md: "250px" }} // Responsive image size
-          desc={
-            "Lindemans Aalst speelt in de Lotto Volley League. Meer informatie over deze competitie kan je hier vinden."
-          }
-          but={"Meer info"}
-          butLink={"https://lottovolleyleague.be/nl/"}
-          isExternal={true} // This is an external link
-        />
-      </Box>
-
-      {/* CEV - External Link */}
-      <Box maxW="60%" width="100%" mb={4}>
-        <CardRow
-          img={"/images/home/website-cev.png"}
-          imgalt={"Website CEV"}
-          imgsize={{ base: "100%", md: "150px" }} // Responsive image size
-          desc={
-            "Informatie over de Europese wedstrijden kan je hier terugvinden."
-          }
-          but={"Meer info"}
-          butLink={"https://www.cev.eu/club/volleyball-cup/2025/men/#competition"}
-          isExternal={true} // This is an external link
-        />
-      </Box>
-
-      {/* Club History */}
-      <Box maxW="60%" width="100%" mb={4}>
-        <CardRow
-          img={"/images/home/clubgeschiedenis.jpg"}
-          imgalt={"Clubgeschiedenis"}
-          imgsize={{ base: "100%", md: "300px" }} // Responsive image size
-          title={"Clubgeschiedenis"}
-          desc={"Lindemans Aalst is een club met een rijke geschiedenis."}
-          but={"Lees de geschiedenis"}
-          butLink={"/clubgeschiedenis"}
-        />
-      </Box>
+      <Flex direction="column" w={{ base: "100%", md: "55%" }} align="center">
+        {[
+          {
+            image: "/images/home/levensloop2024.jpg",
+            title: "Levensloop 2024",
+            description:
+              "Op zaterdag 28 en zondag 29 gaat Levensloop door in het stadspark van Aalst. Ook met de (fan)club nemen we hieraan deel.",
+            link: "/levensloop",
+            imgWidth: { base: "100%", md: "45%" },
+          },
+          {
+            image: "/images/home/gouden-kriekskes-afbeelding.jpg",
+            title: "Gouden Kriekskes 2024",
+            description:
+              "Jaarlijks sluiten we het seizoen af met de uitreiking van prijzen voor beste speler, beste jongere, meest enthousiaste speler en Lifetime Achievement Award.",
+            link: "/goudenkriekskes",
+            imgWidth: { base: "100%", md: "45%" },
+          },
+          {
+            image: "/images/home/021-2024-mei.jpg",
+            title: "'t Blocknotjen",
+            description:
+              "We hebben ook een eigen ledenblad: 't Blocknotjen. Tijdens het seizoen verschijnt dit 1 keer per maand.",
+            link: "/blocknotjen",
+            imgWidth: { base: "50%", md: "25%" },
+          },
+          {
+            image: "/images/home/website-lindemans-aalst.png",
+            title: "Website Lindemans Aalst",
+            description:
+              "Meer informatie over onze club vind je op deze website.",
+            link: "https://www.lindemansaalst.be",
+            isExternal: true,
+            imgWidth: { base: "50%", md: "25%" },
+          },
+          {
+            image: "/images/home/website-volley-league.png",
+            title: "Website Lotto Volley League",
+            description:
+              "Lindemans Aalst speelt in de Lotto Volley League. Meer informatie over deze competitie kan je hier vinden.",
+            link: "https://lottovolleyleague.be/nl/",
+            isExternal: true,
+            imgWidth: { base: "50%", md: "25%" },
+          },
+          {
+            image: "/images/home/website-cev.png",
+            title: "Website CEV",
+            description:
+              "Informatie over de Europese wedstrijden kan je hier terugvinden.",
+            link: "https://www.cev.eu/club/volleyball-cup/2025/men/#competition",
+            isExternal: true,
+            imgWidth: { base: "50%", md: "25%" },
+          },
+          {
+            image: "/images/home/clubgeschiedenis.jpg",
+            title: "Clubgeschiedenis",
+            description:
+              "Lindemans Aalst is een club met een rijke geschiedenis.",
+            link: "/clubgeschiedenis",
+            imgWidth: { base: "100%", md: "45%" },
+          },
+        ].map((card, index) => (
+          <Card
+            key={index}
+            direction={{ base: "column", md: "row" }} // Stacks vertically on mobile and horizontally on desktop
+            overflow="hidden"
+            variant="outline"
+            m={2} // Margin between cards
+          >
+            <Image
+              src={card.image}
+              alt={card.title}
+              mx={{ base: "auto", md: "initial" }}
+              w={card.imgWidth} // Use responsive width
+              borderRadius="md"
+            />
+            <Stack spacing={4} align="start" p={4}>
+              <CardBody>
+                <Heading size="lg">{card.title}</Heading>
+                <Text mt={2}>{card.description}</Text>
+                <Button
+                  as="a"
+                  href={card.link}
+                  target={card.isExternal ? "_blank" : undefined}
+                  rel={card.isExternal ? "noopener noreferrer" : undefined}
+                  mt={4}
+                  background="#f08515"
+                  color="white"
+                  _hover={{ background: "white", color: "#f08515" }}
+                >
+                  {card.isExternal ? "Meer info" : "Ontdek de winnaars"}
+                </Button>
+              </CardBody>
+            </Stack>
+          </Card>
+        ))}
+      </Flex>
     </Flex>
   );
 }
